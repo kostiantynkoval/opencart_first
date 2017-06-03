f3d2835aadd1f231bd1144a1b54d336a6cf100fc
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2017-06-03 11:16:47 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart:81/admin/');
+define('HTTP_CATALOG', 'http://opencart:81/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart:81/admin/');
+define('HTTPS_CATALOG', 'http://opencart:81/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/opencart/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/opencart/system/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/opencart/image/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/opencart/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/opencart/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/opencart/system/config/');
+define('DIR_CACHE', 'C:/OpenServer/domains/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/opencart/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/opencart/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'agkk_');
