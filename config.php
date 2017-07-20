fc48b52701de5c0c235b0ffe99be12da12e0f46d
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2017-07-20 22:39:03 +0300

    Initial project commit

--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://opencart:81/');
 define('HTTPS_SERVER', 'http://opencart:81/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/OpenServer/domains/opencart/catalog/');
-define('DIR_SYSTEM', 'C:/OpenServer/domains/opencart/system/');
-define('DIR_IMAGE', 'C:/OpenServer/domains/opencart/image/');
-define('DIR_LANGUAGE', 'C:/OpenServer/domains/opencart/catalog/language/');
-define('DIR_TEMPLATE', 'C:/OpenServer/domains/opencart/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/OpenServer/domains/opencart/system/config/');
-define('DIR_CACHE', 'C:/OpenServer/domains/opencart/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/OpenServer/domains/opencart/system/storage/download/');
-define('DIR_LOGS', 'C:/OpenServer/domains/opencart/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/OpenServer/domains/opencart/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/OpenServer/domains/opencart/system/storage/upload/');
+define('DIR_APPLICATION', 'D:/OpenServer/domains/opencart/catalog/');
+define('DIR_SYSTEM', 'D:/OpenServer/domains/opencart/system/');
+define('DIR_IMAGE', 'D:/OpenServer/domains/opencart/image/');
+define('DIR_LANGUAGE', 'D:/OpenServer/domains/opencart/catalog/language/');
+define('DIR_TEMPLATE', 'D:/OpenServer/domains/opencart/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/OpenServer/domains/opencart/system/config/');
+define('DIR_CACHE', 'D:/OpenServer/domains/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OpenServer/domains/opencart/system/storage/download/');
+define('DIR_LOGS', 'D:/OpenServer/domains/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OpenServer/domains/opencart/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OpenServer/domains/opencart/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+// Подключаем Gulp и все необходимые библиотеки
+var gulp           = require('gulp'),
+		gutil          = require('gulp-util' ),
+		sass           = require('gulp-sass'),
+		browserSync    = require('browser-sync'),
+		cleanCSS       = require('gulp-clean-css'),
+		autoprefixer   = require('gulp-autoprefixer'),
+		bourbon        = require('node-bourbon'),
+		ftp            = require('vinyl-ftp');
+
+// Обновление страниц сайта на локальном сервере
+gulp.task('browser-sync', function() {
+	browserSync({
+		proxy: "opencart:81",
+		notify: false
+	});
+});
+
+// Компиляция stylesheet.css
+gulp.task('sass', function() {
+	return gulp.src('catalog/view/theme/textile/stylesheet/stylesheet.sass')
+		.pipe(sass({
+			includePaths: bourbon.includePaths
+		}).on('error', sass.logError))
+		.pipe(autoprefixer(['last 15 versions']))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('catalog/view/theme/textile/stylesheet/'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+// Наблюдение за файлами
+gulp.task('watch', ['sass', 'browser-sync'], function() {
+	gulp.watch('catalog/view/theme/textile/stylesheet/stylesheet.sass', ['sass']);
+	gulp.watch('catalog/view/theme/textile/template/**/*.tpl', browserSync.reload);
+	gulp.watch('catalog/view/theme/textile/js/**/*.js', browserSync.reload);
+	gulp.watch('catalog/view/theme/textile/libs/**/*', browserSync.reload);
+});
+
+// Выгрузка изменений на хостинг
+gulp.task('deploy', function() {
+	var conn = ftp.create({
+		host:      'hostname.com',
+		user:      'username',
+		password:  'userpassword',
+		parallel:  10,
+		log: gutil.log
+	});
+	var globs = [
+	'catalog/view/theme/apple/**'
+	];
+	return gulp.src(globs, {buffer: false})
+	.pipe(conn.dest('/path/to/folder/on/server'));
+});
+
+gulp.task('default', ['watch']);
